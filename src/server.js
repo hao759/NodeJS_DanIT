666c88a16c4cb4e8776bf10eb07f7ce352c076ed
hao759 <qhao74154@gmail.com>
2023-04-02 17:02:47 +0700

    swagger faile

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebroute from "./route/web";
 import initAPIRoute from "../src/route/api";
+const swaggerJDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 require("dotenv").config();
 const app = express();
@@ -11,6 +13,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 configViewEngine(app); // confige view
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:9090/",
+      },
+    ],
+  },
+  // apis: ["./server.js"],
+  apis: ["./api.js"],
+};
+const swaggerSpec = swaggerJDoc(options);
+app.use("/api_docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // initWebroute(app); // confige route
 // app.get('/', (req, res) => {
