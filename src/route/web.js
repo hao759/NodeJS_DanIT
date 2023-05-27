d33a8af7f51fb46223eaf4384f84031224a7405f
hao759 <qhao74154@gmail.com>
2023-05-27 17:35:46 +0700

    cbi rs win

--- src/route/web.js
+++ src/route/web.js
@@ -1,8 +1,19 @@
 import express from "express";
 import homeController from "../controller/homeController";
-
-//import getHomepage  from '../controller/homeController';
-
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "/public"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, "-1" + file.originalname);
+  },
+  zzz: function (req, file, cb) {
+    cb(null, req);
+  },
+});
+const upload = multer({ storage });
 let router = express.Router(); //bik dday la route
 
 const initWebroute = (app) => {
@@ -13,10 +24,19 @@ const initWebroute = (app) => {
   router.get("/editUser/:userID", homeController.editUser); //id sai ??
   router.post("/Update-user", homeController.updateUser);
 
+  router.post("/getdetailimg", upload.single("Fileenek"), (req, res) => {
+    console.log("===========");
+
+    console.log(req);
+    console.log(req.file);
+    return res.json({
+      res: req.body,
+    });
+  });
 
   router.post("/testSendMail", homeController.testSendMail);
 
-  // router.get("/uploadFile", homeController.getUploadFilePage);
+  // router.get("/uploadFile", homeController.getUploadFilePage);E:\Code\VisualCode\Node\node-js-basic-hoidanit\src\public\-DuocTonGia.png
   // router.post("/upload-profile-pic", homeController.handleUploadFile);
 
   router.get("/about", (req, res) => {
--- src/server.js
+++ src/server.js
@@ -2,38 +2,43 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebroute from "./route/web";
 import initAPIRoute from "../src/route/api";
-const swaggerJDoc = require("swagger-jsdoc");
-const swaggerUI = require("swagger-ui-express");
-
+// const swaggerJDoc = require("swagger-jsdoc");
+// const swaggerUI = require("swagger-ui-express");
+var bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8079; //backup code
-
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 configViewEngine(app); // confige view
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Node JS API",
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: "http://localhost:9090/",
-      },
-    ],
-  },
-  // apis: ["./server.js"],
-  apis: ["./api.js"],
-};
-const swaggerSpec = swaggerJDoc(options);
-app.use("/api_docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Node JS API",
+//       version: "1.0.0",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:9090/",
+//       },
+//     ],
+//   },
+//   // apis: ["./server.js"],
+//   apis: ["./api.js"],
+// };
+// const swaggerSpec = swaggerJDoc(options);
+// app.use("/api_docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-// initWebroute(app); // confige route
+initWebroute(app); // confige route
 // app.get('/', (req, res) => {
 //     res.render('test/index.ejs')
 // })
